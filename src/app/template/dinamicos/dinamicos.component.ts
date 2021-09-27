1ce71495a3b2e29f745d01ad8910b173fcb8e2ce
Vicman3x3 <victorm.cov@gmail.com>
2021-09-27 16:18:36 -0500

    Template dinámicos
     Se han agregado los formularios de template dinámicos hasta la sección de borrado

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styleUrls: ['./dinamicos.component.css']
+  styleUrls: ['./dinamicos.component.css'],
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+  persona: Persona = {
+    nombre: 'Víctor',
+    favoritos: [
+      { id: 1, nombre: 'King of fighters' },
+      { id: 2, nombre: 'Mario Kart' },
+      { id: 3, nombre: 'Duel Links' },
+    ],
+  };
 
-  constructor() { }
+  @ViewChild('miFormulario') miFormulario!: NgForm;
 
-  ngOnInit(): void {
+  nombreValido(): boolean {
+    return (
+      this.miFormulario?.controls.nombre?.invalid &&
+      this.miFormulario?.controls.nombre?.touched
+    );
   }
 
+  guardar() {
+    console.log('Formulario Posteado');
+  }
+
+  eliminar(index:number){
+    this.persona.favoritos.splice(index,1);
+  }
 }
