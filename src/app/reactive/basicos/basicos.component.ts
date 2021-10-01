044bd892886f62d04c8fb2a1dcc31fe0d2ad1331
Vicman3x3 <victorm.cov@gmail.com>
2021-10-01 16:31:20 -0500

    FormBuilder
      Se empezaron los reactivos con builder

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.css']
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent  {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
-  }
+  // miFormulario: FormGroup = new FormGroup({
+  //   'nombre':       new FormControl('RTX 4080'),
+  //   'precio':       new FormControl(1500),
+  //   'existencias':  new FormControl(5)
+  // });
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['RTX 4080ti'],
+    precio: [0],
+    existencias: [0]
+  })
 
 }
